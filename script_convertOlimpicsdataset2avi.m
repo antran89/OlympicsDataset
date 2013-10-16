2ad39f9ad8635b87d99fc4a468a86d31bb8f8751
howtobeahacker <tranlaman@gmail.com>
2013-10-16 17:42:49 +0800

    NUS convert sequence into avi file

---
+++ script_convertOlimpicsdataset2avi.m
@@ -0,0 +1,7 @@
+clear;clc
+
+% initalize optsdataset
+optsdataset.seqdata = 'D:\A0081742H\data\OlympicSports\videos';
+optsdataset.folder = 'D:\A0081742H\data\OlympicSports_avi\videos';
+%seq2avi(optsdataset)
+nusseq2avi(optsdataset)
